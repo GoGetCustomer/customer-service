46b2934e4fe95404991fee26d95a9d186b925225
Hm-source <swo8852@gmail.com>
2025-01-17 12:08:25 +0900

    feat: 컨트롤러 내 스케쥴 로직 서비스 레이어로 이동 & 스케쥴 작업 상태 추가 #3

--- customer-service/src/main/java/com/example/customerservice/contents/service/ContentsSchedulerService.java
+++ customer-service/src/main/java/com/example/customerservice/contents/service/ContentsSchedulerService.java
@@ -2,29 +2,36 @@ package com.example.customerservice.contents.service;
 
 import com.example.customerservice.contents.client.BookApiClient;
 import com.example.customerservice.contents.entity.Contents;
+import com.example.customerservice.contents.entity.SchedulerStatus;
 import com.example.customerservice.contents.repository.ContentsRepository;
 import com.example.customerservice.contents.util.AladinUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
+import java.util.concurrent.ScheduledFuture;
 
 import static com.example.customerservice.contents.util.AladinUtils.calculateTotalPages;
 import static com.example.customerservice.contents.util.AladinUtils.parseTotalResults;
+
 
 @Service
 @RequiredArgsConstructor
 public class ContentsSchedulerService {
     private final BookApiClient bookApiClient;
     private final ContentsRepository contentsRepository;
+    private final ThreadPoolTaskScheduler taskScheduler;
+    private ScheduledFuture<?> scheduledTask;
 
 
-    private Integer checkTotalPages(int maxResults) throws JsonProcessingException {
+    public Integer checkTotalPages(int maxResults) throws JsonProcessingException {
         String response = bookApiClient.fetchBooksByPage(1, maxResults);
         int totalResults = parseTotalResults(response);
         return calculateTotalPages(totalResults, maxResults);
@@ -51,7 +58,7 @@ public class ContentsSchedulerService {
         System.out.println("Execution time: " +  timeElapsed.toSeconds() + " seconds");
     }
 
-    private void saveContentsToDatabase(List<Contents> contentsList) {
+    void saveContentsToDatabase(List<Contents> contentsList) {
         List<String> existingTitlesAndWriters = contentsRepository.findAllTitlesAndWriters();
 
         List<Contents> newContents = contentsList.stream()
@@ -62,4 +69,48 @@ public class ContentsSchedulerService {
     }
 
 
+
+    public SchedulerStatus startBookSync(int maxResults) {
+        if (scheduledTask != null && !scheduledTask.isCancelled()) {
+            return SchedulerStatus.ALREADY_RUNNING;
+        }
+
+        scheduledTask = taskScheduler.schedule(() -> {
+            try {
+                fetchBooksForAllPages(maxResults);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("JSON 처리 중 오류 발생", e);
+            }
+        }, triggerContext -> {
+            CronTrigger cronTrigger = new CronTrigger("0 0 0 ? * WED");
+            return cronTrigger.nextExecutionTime(triggerContext).toInstant();
+        });
+
+        /*
+        1분마다 실힝 (테스트용)
+
+        scheduledTask = taskScheduler.scheduleAtFixedRate(() -> {
+            try {
+                fetchBooksForAllPages(maxResults);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("Error while processing JSON", e);
+            }
+        }, Duration.ofMinutes(1));
+         */
+
+
+        return SchedulerStatus.STARTED;
+    }
+
+    public SchedulerStatus stopBookSync() {
+        if (scheduledTask != null) {
+            scheduledTask.cancel(true);
+            return SchedulerStatus.STOPPED;
+        }
+        return SchedulerStatus.NO_TASK_RUNNING;
+    }
+
+    public boolean isTaskRunning() {
+        return scheduledTask != null && !scheduledTask.isCancelled();
+    }
 }
