36883278405ba1848555ce3f2c972fe84bad7799
Hm-source <swo8852@gmail.com>
2025-01-17 10:10:24 +0900

    feat: 작업을 추가/중지하거나 동적으로 스케줄을 변경하기 위해 빈 설정. #3

---
+++ customer-service/src/main/java/com/example/customerservice/config/SchedulerConfig.java
@@ -0,0 +1,17 @@
+package com.example.customerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(3);
+        scheduler.setThreadNamePrefix("BookUpdateScheduler-");
+        return scheduler;
+    }
+}
